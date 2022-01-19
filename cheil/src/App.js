864c66a1b9645ad9001d497b4f834855735cec63
hmiller020414 <hayleymm14@gmail.com>
2022-01-19 20:53:40 +0100

    create component and services folders. create Data.js within services folder, do simple fetch and console.log of data within App.js

--- cheil/src/App.js
+++ cheil/src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+import ProductData from './services/Data';
+
+class App extends Component {
+
+  async componentDidMount() {
+    const products = await ProductData.index()
+    console.log(products)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
+  
 }
+
 
 export default App;
